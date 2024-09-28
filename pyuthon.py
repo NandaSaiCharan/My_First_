882417c118f1dda5223b605e3eb50ab1a1d68c41
NandaSaiCharan <nanda.sai.charan.999@gmail.com>
2024-09-28 12:19:50 +0530

    Initial  Web

---
+++ pyuthon.py
@@ -0,0 +1,19 @@
+# # object ante any thing stores values:-
+# # int=1 indhulo +,-,0 store chesukochu
+# # float=1.2 indhulo all decimal values
+# # complex=2+i3 all imaginary values
+# # str=('  ',"   ",'''   ''',"""    """) iekkada mention chesina ani qutation mark forms lo store chesukochu
+# # list=[12,"nds",1+i2] list tuple both same list [] lo untadhi changes chesukochu
+# # tuple=(12,"nds",1+i2)
+# # set={12,"nds",1+i2}
+# # range=(1:10:1++)
+# # frozenset=frozenset(12,"nds",1+i2)
+# # dict{wssnwsj:1,qsdsd:2}
+# # bool= True,False
+# # byte=byte(122,1122)
+# # arraybyte=
+# # memoryvalue=
+# list lo manam challa operation chaiyachu 1 indexing 2 adding 3 deleting 
+lst=[1,2,"nanda"]
+b=lst.append(0)
+print(b)
